02c479a09cbaf912a2018313d1632a6091807d95
Neil Seejoor <nvseejoor@gmail.com>
2018-07-24 21:53:04 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params)
+	  @post = Post.new(title: params[:post][:title], description: params[:post][:description])
 	  @post.save
 	  redirect_to post_path(@post)
 	end
